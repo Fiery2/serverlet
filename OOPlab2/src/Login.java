f94d3729e87a13c31efc3c99e625fb8a7e163fc7
Fiery <jack@ponder-it.com>
2020-01-21 16:56:08 -0500

    1

---
+++ OOPlab2/src/Login.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jack
+ */
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+@WebServlet(urlPatterns={"/login"})
+public class Login extends HttpServlet
+{
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+        resp.setContentType("text/plain");
+        var pw = resp.getWriter();
+        var name = req.getParameter("user");
+        if( name == null ){
+            pw.printf("No username provided");
+        } else {
+            var sess = req.getSession();
+            sess.setAttribute("name", name );
+            pw.printf("Logged in as "+name);
+        }
+    }
+
+}
---
+++ OOPlab2/src/Logout.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jack
+ */
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+@WebServlet(urlPatterns={"/logout"})
+public class Logout extends HttpServlet
+{
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+        req.getSession().removeAttribute("name");
+        resp.setContentType("text/plain");
+        var pw = resp.getWriter();
+        pw.printf("Logged out");
+    }
+
+}
